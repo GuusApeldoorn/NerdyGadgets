f8dc3ede91280c488a40b4dcfb907211eb61711e
Gerrit Koekoek <s1129529@windesheim.nl>
2019-05-24 13:37:21 +0200

    Update JDialogfw.java

--- ProjectKBSD/src/JDialogs/JDialogfw.java
+++ ProjectKBSD/src/JDialogs/JDialogfw.java
@@ -82,6 +82,7 @@ public class JDialogfw extends JDialog {
         JLabel1 = new JLabel();
         JLabel1.setText("Beschikbaar");
         panel2.add(JLabel1, new com.intellij.uiDesigner.core.GridConstraints(0, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+            setResizable(false);
     }
 
     /**
