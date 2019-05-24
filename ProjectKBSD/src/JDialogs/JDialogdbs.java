dcb1f4fdf5bfb12dd278142f647f17aafaa64a99
Gerrit Koekoek <s1129529@windesheim.nl>
2019-05-24 13:37:17 +0200

    Update JDialogdbs.java

--- ProjectKBSD/src/JDialogs/JDialogdbs.java
+++ ProjectKBSD/src/JDialogs/JDialogdbs.java
@@ -267,6 +267,7 @@ public class JDialogdbs extends JDialog {
         JLabel7 = new JLabel();
         JLabel7.setText("Beschikbaar");
         panel4.add(JLabel7, new com.intellij.uiDesigner.core.GridConstraints(0, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+            setResizable(false);
     }
 
     /**
